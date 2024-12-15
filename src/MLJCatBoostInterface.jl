c4a97cefe2ef9667877fe2f1cbaff593a3b89d66
Tyler Thomas <tylerjthomas9@gmail.com>
2024-12-15 12:33:29 -0700

    fix formatting

--- src/MLJCatBoostInterface.jl
+++ src/MLJCatBoostInterface.jl
@@ -140,7 +140,7 @@ end
 #####
 
 function MMI.update(mlj_model::CatBoostModels, verbosity::Integer, fitresult, cache,
-    data_pool)
+                    data_pool)
     if mlj_model.iterations > cache.mlj_model.iterations &&
         MMI.is_same_except(mlj_model, cache.mlj_model, :iterations)
         iterations = mlj_model.iterations - cache.mlj_model.iterations
